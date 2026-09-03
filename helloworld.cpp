c274193251750854e1293a022589e41d6ed91761
Prem Kumar M <premkumar.prem09876@gmail.com>
2026-09-03 22:21:24 +0530

    My first file uploaded to git in c++

---
+++ helloworld.cpp
@@ -0,0 +1,19 @@
+// This line includes the iostream library.
+// iostream provides functions for taking input and displaying output.
+#include <iostream>
+
+// This allows us to use names like cout without writing std::cout.
+using namespace std;
+
+// Every C++ program starts execution from the main() function.
+int main() {
+
+    // cout is used to print/output text on the screen.
+    // << is called the insertion operator and sends the text to cout.
+    // endl moves the cursor to the next line.
+    cout << "Hello, World!" << endl;
+
+    // return 0 tells the operating system that the program
+    // finished successfully without any errors.
+    return 0;
+}
