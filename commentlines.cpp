8e34695db90239188dc074af7ceaf7b0c007eaee
Prem Kumar M <premkumar.prem09876@gmail.com>
2026-09-03 22:32:27 +0530

    Comment lines declaration and uses.

---
+++ commentlines.cpp
@@ -0,0 +1,28 @@
+// ============================================================
+// C++ Learning Journey
+// Topic: Comments
+// ============================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // This is a single-line comment.
+    // The compiler ignores everything written after //.
+    cout << "Hello, C++!" << endl;
+
+    /*
+        This is a multi-line comment.
+
+        You can write multiple lines inside
+        opening and closing comment markers
+
+        Multi line comments are useful when
+        you need to explain something in detail.
+    */
+
+    cout << "I am learning C++ step by step." << endl;
+
+    return 0;
+}
